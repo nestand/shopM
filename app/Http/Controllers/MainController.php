09d63827a94301ca6dd5d23771b0cb36153029af
Andrey NESTEROV <andrey.nesterov@laposte.net>
2021-06-16 13:36:25 +0200

    3 views created for main, categories and product page; in web.php 3 routes added to access these pages

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//command -> php artisan make:controller MainController
+class MainController extends Controller
+{
+    //
+}
