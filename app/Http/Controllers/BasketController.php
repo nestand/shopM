506e4a385adeeb3e9a77ef6a3fc96952b8f8c772
Andrey NESTEROV <andrey.nesterov@laposte.net>
2021-06-24 15:08:28 +0200

    basket in beta. fixed default value for status in DB, created session for the orderId 'add product' function tested

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -10,25 +10,31 @@ class BasketController extends Controller
     // the cart
     public function basket()
     {
-        return view('basket');
+        $orderId=session('orderId');
+        return view('basket', compact('order'));
     }
     public function basketPlace()
     {
         return view('order');
+        if (!is_null($orderId)){
+            $order=findOrFail($orderId);
+        }
     }
     /*Fixed Method App\Http\Controllers\BasketController::basketAdd does not exist with this function.
     The session added.
     */
     public function basketAdd($productId){
 $orderId = session('orderId');
-/*check for orderId*/
+/*check for orderId if not create one and keep the id*/
     if (is_null($orderId)){
         $order=Order::create();
         session(['orderId'=>$order->id]);
     }else{
         $order=Order::find($orderId);
     }
-    dump($order);
-
+    //to put the article in the cart and send it to the order table. TEST OK!
+    $order->products()->attach($productId);
+    //dump($order->products);
+return view('basket', compact('order'));
     }
 }
--- database/migrations/2021_06_24_121244_update_create_orders_table.php
+++ database/migrations/2021_06_24_121244_update_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class UpdateCreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,7 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        /*cart table*/
-        Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            $table->tinyInteger('status')->default(0)->change();
-            $table->string('phone')->nullable();
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -29,6 +23,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        //
     }
 }
